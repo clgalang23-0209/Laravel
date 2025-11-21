968c9279cc76b543568f65a6513da17eafb8497a
clgalang23-0209 <christiangalang61@gmail.com>
2025-11-21 16:18:10 +0800

    latest nov 21 with login form
    hihi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\controllers\AuthController;
 
-    Route::get('/', function () {
+Route::get('/', function () {
     return view('portfolio');
+
+    
+Route::get('register',[AuthController::class,'ShowRegister'])->name 
+('register.form');
+Route::post('register',[AuthController::class,'register'])->name     
+('register');
+Route::get('login',[AuthController::class,'Showlogin'])->name ('login.form');
+Route::get('login',[AuthController::class,'login'])->name ('login');
+
+
+Route::get('dashboard',function(){
+    return view('dashboard');
+})->middleware('auth')->name('dashboard ');
 });
