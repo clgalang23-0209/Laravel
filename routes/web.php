e0af2375fe85a392e4a7e1f2975ebe9771b67e6e
clgalang23-0209 <christiangalang61@gmail.com>
2025-11-21 16:21:30 +0800

    sebastian
    wow

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\http\controllers\AuthController;
+use sebastianBergmann\CodeCoverage\Report\Html\Dashboard
 
 Route::get('/', function () {
     return view('portfolio');
-
     
+});
 Route::get('register',[AuthController::class,'ShowRegister'])->name 
 ('register.form');
 Route::post('register',[AuthController::class,'register'])->name     
@@ -18,4 +19,3 @@ Route::get('login',[AuthController::class,'login'])->name ('login');
 Route::get('dashboard',function(){
     return view('dashboard');
 })->middleware('auth')->name('dashboard ');
-});
